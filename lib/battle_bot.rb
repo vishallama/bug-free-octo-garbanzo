236a76bdd51f0b47dae55f1724c58bb7a1ae55b1
Vishal Lama <vishal.lama@gmail.com>
2016-07-16 19:27:08 -0600

    Pass almost all the tests

--- lib/battle_bot.rb
+++ lib/battle_bot.rb
@@ -3,6 +3,7 @@ require 'weapon'
 
 class BattleBot
   @@weapons_picked_up = []
+  @@count = 0
 
   attr_reader :name, :health, :enemies, :weapon
 
@@ -13,6 +14,7 @@ class BattleBot
     @enemies = []
     @weapon = nil
     @has_weapon = false
+    @@count += 1
   end
 
   def dead?
@@ -23,23 +25,67 @@ class BattleBot
     @has_weapon
   end
 
+  def attack(other)
+    raise ArgumentError if !other.instance_of? BattleBot 
+    raise ArgumentError if other == self
+    raise ArgumentError if !has_weapon?
+
+    other.receive_attack_from(self)
+  end
+
+  def receive_attack_from(other)
+    raise ArgumentError if !other.instance_of? BattleBot 
+    raise ArgumentError if other == self
+    raise ArgumentError if !other.has_weapon?
+
+    take_damage(other.weapon.damage)
+    enemies << other if !enemies.include? other
+
+    defend_against(other)
+  end
+
+  def defend_against(other)
+    dead?
+    has_weapon?
+  end
+
   def pick_up(new_weapon)
     raise ArgumentError if !new_weapon.instance_of?(Weapon)
     raise ArgumentError if @@weapons_picked_up.include?(new_weapon)
 
-    @weapon = new_weapon
-    @has_weapon = true
-    @@weapons_picked_up << new
+    if weapon.nil?
+      @weapon = new_weapon
+      @has_weapon = true
+      @@weapons_picked_up << new_weapon 
+      new_weapon.bot = self
+      new_weapon
+    else
+      nil
+    end
+  end
+
+  def drop_weapon
+    @weapon.bot = nil
+    @weapon = nil
+    @has_weapon = false
   end
 
   def take_damage(damage)
-    return ArgumentError if !damage.instance_of?(Fixnum)
+    raise ArgumentError if !damage.instance_of?(Fixnum)
 
     @health = (@health - damage) < 0 ? 0 : (health - damage)
   end
 
   def heal
-    @health = (@health + 10) > 100 ? 100 : (health + 10)
+    if health == 0
+      return
+    else
+      @health = (@health + 10) > 100 ? 100 : (health + 10)
+    end
+  end
+
+  def count
+    @@count
   end
 
 end
--- lib/weapon.rb
+++ lib/weapon.rb
@@ -1,11 +1,28 @@
 # weapon.rb
 
 class Weapon
-  attr_reader :name
+  attr_reader :name, :damage
+  attr_accessor :bot
 
-  def initialize(name, damage = nil)
+  def initialize(name, damage = 0)
+    raise ArgumentError if !name.instance_of? String
+    raise ArgumentError if !damage.instance_of? Fixnum
     @name = name
     @damage = damage
+    @bot = nil
+    @picked_up = false
+  end
+
+  def bot=(some_bot)
+    if some_bot.nil? || (some_bot.instance_of? BattleBot)
+      @bot = some_bot
+    else
+      raise_error ArgumentError
+    end
+  end
+
+  def picked_up?
+    @picked_up
   end
 end
 
